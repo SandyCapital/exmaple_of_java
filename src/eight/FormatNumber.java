890af42d0e40af5dbee5d88fd85cc1e6819751fa
刘鹏 <1056172039qq.com>
2018-10-19 00:04:29 +0800

    swing

--- src/eight/FormatNumber.java
+++ src/eight/FormatNumber.java
@@ -1,5 +1,7 @@
 package eight;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class FormatNumber {
@@ -8,5 +10,13 @@ public class FormatNumber {
         System.out.println("请输入一个数字");
         double number=scanner.nextDouble();
         System.out.println("该数字用Locale类的常量作为格式化对象的构造函数，将获得的");
+        NumberFormat format=NumberFormat.getCurrencyInstance(Locale.CHINA);
+        System.out.println("Locale.CHINA: "+format.format(number));
+        format=NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("Locale.US: "+format.format(number));
+        format=NumberFormat.getCurrencyInstance(Locale.ENGLISH);
+        System.out.println("Locale.ENGLISH: "+format.format(number));
+        format=NumberFormat.getCurrencyInstance(Locale.TAIWAN);
+        System.out.println("Locale.TAIWAN: "+format.format(number));
     }
 }
